8fc8c12a51fca7e0ee2fd34eb583b933e998182c
Eureka0 <dblxl33236@gmail.com>
2025-11-23 22:30:25 +0800

    fixed test issues

--- tests/test_rinex.py
+++ tests/test_rinex.py
@@ -8,15 +8,16 @@ from gnss_tec import read_rinex_obs
 
 def test_read_rinex_obs(rinex_obs_v3_hatanaka, rinex_obs_v3):
     start = perf_counter()
-    df_hatanaka = read_rinex_obs(rinex_obs_v3_hatanaka)
+    header1, df_hatanaka = read_rinex_obs(rinex_obs_v3_hatanaka)
     end = perf_counter()
     logging.info("Time taken to read RINEX .crx.gz file: %.2f seconds", end - start)
 
     start = perf_counter()
-    df = read_rinex_obs(rinex_obs_v3)
+    header2, df = read_rinex_obs(rinex_obs_v3)
     end = perf_counter()
     logging.info("Time taken to read RINEX .rnx file: %.2f seconds", end - start)
 
+    assert header1 == header2
     assert_frame_equal(df_hatanaka, df, check_exact=False, abs_tol=1e-8)
     assert df.shape[0] > 0 and df.shape[1] > 0
     assert "Time" in df.columns and "PRN" in df.columns

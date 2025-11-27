a9a5183162c4d45ae9ca5218ddda680e7e8f5fc9
Eureka0 <dblxl33236@gmail.com>
2025-11-27 17:05:38 +0800

    chore(tec): add more codes

--- gnss_tec/tec/constants.py
+++ gnss_tec/tec/constants.py
@@ -25,10 +25,16 @@ SIGNAL_FREQ = {
 }
 """Signal frequencies for supported constellations and signals in Hz."""
 
-C1_CODES = {"C": ["C2I", "C2X", "C1X", "C2W", "C1C"], "G": ["C1W", "C1C", "C1X"]}
+C1_CODES = {
+    "C": ["C2I", "C2D", "C2X", "C1I", "C1D", "C1X", "C2W", "C1C"],
+    "G": ["C1W", "C1C", "C1X"],
+}
 """Observation codes priority list for C1 measurements."""
 
-C2_CODES = {"C": ["C6I", "C7I", "C7D", "C5X"], "G": ["C2W", "C2X"]}
+C2_CODES = {
+    "C": ["C6I", "C6D", "C6X", "C7I", "C7D", "C7X", "C5I", "C5D", "C5X"],
+    "G": ["C2W", "C2C", "C2X", "C5W", "C5C", "C5X"],
+}
 """Observation codes priority list for C2 measurements."""
 
 DEFAULT_IPP_HEIGHT = 400e3

d9287384d1b8956fa05560a73f98076e91c65e28
Eureka0 <dblxl33236@gmail.com>
2025-11-28 17:56:29 +0800

    chore: add `read_bias` imports

--- gnss_tec/__init__.py
+++ gnss_tec/__init__.py
@@ -1,4 +1,4 @@
 from .rinex import read_rinex_obs
-from .tec import calc_tec
+from .tec import calc_tec, read_bias
 
-__all__ = ["read_rinex_obs", "calc_tec"]
+__all__ = ["read_rinex_obs", "calc_tec", "read_bias"]

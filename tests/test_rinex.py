015376021d6957ffb5c49f1b9c1bf5b4cdf615fa
Eureka0 <dblxl33236@gmail.com>
2025-12-01 20:11:09 +0800

    fix(test): fix an error due to LazyFrame return value

--- tests/test_rinex.py
+++ tests/test_rinex.py
@@ -4,8 +4,11 @@ from gnss_tec import read_rinex_obs
 
 
 def test_read_rinex_obs(rinex_obs_v3_hatanaka, rinex_obs_v3):
-    header1, df_hatanaka = read_rinex_obs(rinex_obs_v3_hatanaka)
-    header2, df = read_rinex_obs(rinex_obs_v3)
+    header1, lf_hatanaka = read_rinex_obs(rinex_obs_v3_hatanaka)
+    header2, lf = read_rinex_obs(rinex_obs_v3)
+
+    # df_hatanaka = lf_hatanaka.collect()
+    df = lf.collect()
 
     assert header1 == header2
     # assert_frame_equal(df_hatanaka, df, check_exact=False, abs_tol=1e-8)

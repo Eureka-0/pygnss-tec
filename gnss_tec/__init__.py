ba12a63546ffac814c9438c82185618eafc38d2e
Eureka0 <dblxl33236@gmail.com>
2025-12-03 16:36:32 +0800

    chore: add `calc_tec_from_df` imports

--- gnss_tec/__init__.py
+++ gnss_tec/__init__.py
@@ -1,4 +1,4 @@
 from .rinex import read_rinex_obs
-from .tec import calc_tec, read_bias
+from .tec import calc_tec, calc_tec_from_df, read_bias
 
-__all__ = ["read_rinex_obs", "calc_tec", "read_bias"]
+__all__ = ["read_rinex_obs", "calc_tec", "calc_tec_from_df", "read_bias"]
--- gnss_tec/tec/__init__.py
+++ gnss_tec/tec/__init__.py
@@ -1,10 +1,11 @@
 from .bias import read_bias
 from .constants import SUPPORTED_CONSTELLATIONS, SUPPORTED_RINEX_VERSIONS
-from .tec_calculation import calc_tec
+from .tec_calculation import calc_tec, calc_tec_from_df
 
 __all__ = [
     "SUPPORTED_CONSTELLATIONS",
     "SUPPORTED_RINEX_VERSIONS",
     "calc_tec",
+    "calc_tec_from_df",
     "read_bias",
 ]

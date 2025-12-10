6c19d01579612f2cf52f6ef966baf30ba0bf8796
Eureka0 <dblxl33236@gmail.com>
2025-12-10 16:48:08 +0800

    test: add more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,16 @@ def test_data_dir():
 
 
 @fixture
+def rinex_obs_v2(test_data_dir):
+    return test_data_dir / "rinex_obs_v2/bele0100.24o.gz"
+
+
+@fixture
+def rinex_nav_v2(test_data_dir):
+    return test_data_dir / "rinex_nav_v2/brdc0100.24g.gz"
+
+
+@fixture
 def rinex_obs_v3_hatanaka(test_data_dir):
     return test_data_dir / "rinex_obs_v3/CIBG00IDN_R_20240100000_01D_30S_MO.crx.gz"
 
@@ -16,3 +26,13 @@ def rinex_obs_v3_hatanaka(test_data_dir):
 @fixture
 def rinex_obs_v3(test_data_dir):
     return test_data_dir / "rinex_obs_v3/CIBG00IDN_R_20240100000_01D_30S_MO.rnx.gz"
+
+
+@fixture
+def rinex_nav_v3(test_data_dir):
+    return test_data_dir / "rinex_nav_v3/BRDC00IGS_R_20240100000_01D_MN.rnx.gz"
+
+
+@fixture
+def bias(test_data_dir):
+    return test_data_dir / "bias/CAS0OPSRAP_20240100000_01D_01D_DCB.BIA.gz"
--- tests/test_rinex.py
+++ tests/test_rinex.py
@@ -1,16 +1,45 @@
-# from polars.testing import assert_frame_equal
+from polars.testing import assert_frame_equal
 
-from gnss_tec import read_rinex_obs
+import gnss_tec as gt
 
 
-def test_read_rinex_obs(rinex_obs_v3_hatanaka, rinex_obs_v3):
-    header1, lf_hatanaka = read_rinex_obs(rinex_obs_v3_hatanaka)
-    header2, lf = read_rinex_obs(rinex_obs_v3)
+def test_read_rinex_obs_v2(rinex_obs_v2, rinex_nav_v2):
+    header, lf = gt.read_rinex_obs(rinex_obs_v2, rinex_nav_v2)
+    df = lf.collect()
 
-    # df_hatanaka = lf_hatanaka.collect()
+    assert header.version.startswith("2.")
+    assert header.sampling_interval == 15
+    assert df.shape[0] > 0 and df.shape[1] > 0
+    assert {
+        "time",
+        "station",
+        "prn",
+        "azimuth",
+        "elevation",
+        "C1",
+        "L1",
+        "S1",
+    }.issubset(set(df.columns))
+
+
+def test_read_rinex_obs_v3(rinex_obs_v3_hatanaka, rinex_obs_v3, rinex_nav_v3):
+    header1, lf_hatanaka = gt.read_rinex_obs(rinex_obs_v3_hatanaka, rinex_nav_v3)
+    header2, lf = gt.read_rinex_obs(rinex_obs_v3, rinex_nav_v3)
+    df_hatanaka = lf_hatanaka.collect()
     df = lf.collect()
 
     assert header1 == header2
-    # assert_frame_equal(df_hatanaka, df, check_exact=False, abs_tol=1e-8)
+    assert header1.version.startswith("3.")
+    assert header1.sampling_interval == 30
+    assert_frame_equal(df_hatanaka, df, check_exact=False, abs_tol=1e-8)
     assert df.shape[0] > 0 and df.shape[1] > 0
-    assert "time" in df.columns and "prn" in df.columns
+    assert {
+        "time",
+        "station",
+        "prn",
+        "azimuth",
+        "elevation",
+        "C1C",
+        "L1C",
+        "S1C",
+    }.issubset(set(df.columns))

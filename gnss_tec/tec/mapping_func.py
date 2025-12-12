f40b8d80e432d39b5a70f6db6a73ff0808d02b61
Eureka0 <dblxl33236@gmail.com>
2025-12-12 14:53:44 +0800

    refactor: add `MappingFunctionProtocol` for mapping function implementations

--- gnss_tec/tec/mapping_func.py
+++ gnss_tec/tec/mapping_func.py
@@ -1,7 +1,38 @@
+from typing import Protocol
+
 import numpy as np
 import polars as pl
 
 from .constants import Re, TECConfig
+
+
+class MappingFunctionProtocol(Protocol):
+    def __call__(
+        self,
+        azimuth: pl.Expr,
+        elevation: pl.Expr,
+        rx_lat_deg: pl.Expr,
+        rx_lon_deg: pl.Expr,
+        config: TECConfig,
+    ) -> tuple[pl.Expr, pl.Expr, pl.Expr]:
+        """
+        Calculate the mapping function and Ionospheric Pierce Point (IPP) latitude and
+        longitude.
+
+        Args:
+            azimuth (pl.Expr): Satellite azimuth angle in degrees.
+            elevation (pl.Expr): Satellite elevation angle in degrees.
+            rx_lat_deg (pl.Expr): Receiver latitude in degrees.
+            rx_lon_deg (pl.Expr): Receiver longitude in degrees.
+            config (TECConfig): Configuration parameters.
+
+        Returns:
+            (pl.Expr, pl.Expr, pl.Expr): A tuple containing,
+                - Mapping function (pl.Expr)
+                - IPP latitude in degrees (pl.Expr)
+                - IPP longitude in degrees (pl.Expr)
+        """
+        ...
 
 
 def single_layer_model(
@@ -11,22 +42,6 @@ def single_layer_model(
     rx_lon_deg: pl.Expr,
     config: TECConfig,
 ) -> tuple[pl.Expr, pl.Expr, pl.Expr]:
-    """
-    Calculate the mapping function and Ionospheric Pierce Point (IPP) latitude and
-    longitude using the Single Layer Model (SLM).
-
-    Args:
-        azimuth (pl.Expr): Satellite azimuth angle in degrees.
-        elevation (pl.Expr): Satellite elevation angle in degrees.
-        rx_lat_deg (pl.Expr): Receiver latitude in degrees.
-        rx_lon_deg (pl.Expr): Receiver longitude in degrees.
-
-    Returns:
-        (pl.Expr, pl.Expr, pl.Expr): A tuple containing,
-            - Mapping function (pl.Expr)
-            - IPP latitude in degrees (pl.Expr)
-            - IPP longitude in degrees (pl.Expr)
-    """
     az = azimuth.radians()
     el = elevation.radians()
     rx_lat = rx_lat_deg.radians()
